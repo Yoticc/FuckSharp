3891f979afee2718c4358c6536740b86e71eb0ec
MrYotic <yoticcitoy@gmail.com>
2022-05-20 03:42:03 +0300

    FuckSharp DON'T OPEN BF

--- FuckSharp/brain.cs
+++ FuckSharp/brain.cs
@@ -201,31 +201,23 @@ public static class FuckSharp
         public byte[] Execute(string code, int allocSize = 30000, int pos = 0)
         {
             byte[] bytes = new byte[allocSize];
-            Dictionary<int, int> point = new Dictionary<int, int> { { -1, 0 }, { 0, 0 } };
-            int power = 0;
-            Dictionary<char, Action> types = new()
+            Dictionary<int, int> point = new Dictionary<int, int> { { -9, 0 }, { -2, 0}, { -1, 0 }, { 0, 0 } };
+            Dictionary<byte, Action> types = new()
             {
-                { '>', new(() => pos++) }, { '<', new(() => pos-- ) },
-                { '+', new(() => bytes[pos]++ ) }, { '-', new(() => bytes[pos]--) },
-                { '.', new(() => Console.Write((char)bytes[pos])) }, { ',', new(() => bytes[pos] = (byte)Console.ReadLine()[0]) },
-                { '[', new(() => {
+                { 62, new(() => pos++) }, { 60, new(() => pos-- ) },
+                { 43, new(() => bytes[pos]++ ) }, { 45, new(() => bytes[pos]--) },
+                { 46, new(() => Console.Write((char)bytes[pos])) }, { 44, new(() => bytes[pos] = (byte)Console.ReadLine()[0]) },
+                { 91, new(() => {
                     if (bytes[pos] == 0)
-                        do point[-1] += (code[++point[power] - 1] == '[' ? 1 : (code[point[power] - 1] == ']' ? -1 : 0));
-                        while (code[point[power] - 1] != ']' || point[-1] != 0);
-                    else point[++power] = point[power - 1] + 1;
-                    point[power]--;
+                        do point[-1] += (code[++point[point[-2]] - 1] == 91 ? 1 : (code[point[point[-2]] - 1] == 93 ? -1 : 0));
+                        while (code[point[point[-2]] - 1] != 93 || point[-1] != 0);
+                    else point[++point[-2]] = point[point[-2] - 1] + 1;
+                    point[point[-2]]--;
                 })},
-                { ']', new(() => {
-                    if (bytes[pos] != 0) point[power] = point[power-1]+1;
-                    else power--;
-                    point[power]--;
-                })}
+                { 93, new(() => point[-9] = (bytes[pos] != 0 ? new(() => point[point[-2]] = point[point[-2] - 1] + 1) : new Action(() => point[-2]--))() is int ? point[point[-2]]-- : point[point[-2]]--)}
             };
-            while (point[power] < code.Length)
-            {
-                types[code[point[power]]]();                
-                point[power]++;
-            }
+            while (point[point[-2]] < code.Length)
+                point[-9] = types[(byte)code[point[point[-2]]]]() is double ? point[point[-2]]++ : point[point[-2]]++;                
             return bytes;
         }
     }
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,6 +1,2 @@
 ﻿using static FuckSharp;
 new SBF().Execute(">++++[>++++[<<++++>>-]<-]<+.", 8);
-byte[] bytes = new byte[5] { 0, 1, 2, 3, 4};
-int pos = 2;
-//Console.WriteLine(bytes[pos]);
-int d = 0;
